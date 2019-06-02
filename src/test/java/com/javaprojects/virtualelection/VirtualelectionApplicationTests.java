240eeae6f8572bae9fa33f332a4a351b8b55e173
Chirag <chiraglosalka@gmail.com>
2019-06-02 09:24:26 +0530

    First Commit

---
+++ src/test/java/com/javaprojects/virtualelection/VirtualelectionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javaprojects.virtualelection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VirtualelectionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
